055f15d945a25e91c603bc9a311d322758440a41
Le0wang06 <098leowang@gmail.com>
2025-05-18 21:35:15 -0700

    init commit

---
+++ PlayerManager.ts
@@ -0,0 +1,17 @@
+import * as hz from 'horizon/core';
+
+class PlayerManager extends hz.Component<typeof PlayerManager> {
+  static propsDefinition = {};
+  private playerMap: Map<number, hz.Player> = new Map<number, hz.Player>();
+
+  start() {
+    this.connectCodeBlockEvent(
+      this.entity,
+      hz.CodeBlockEvents.OnPlayerEnterWorld,
+      (player: hz.Player) => {
+        this.playerMap.set(player.id, player);
+      },
+    );
+  }
+}
+hz.Component.register(PlayerManager);
